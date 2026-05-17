14bd19ee03a974380768c4a08c3dfb42e4638f95
cynexicb <cynex.ctf@gmail.com>
2026-05-17 15:00:42 +0530

    updated logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
+
+import aietLogo from "../assets/aiet-logo.png";
+
 function Header() {
   return (
-    <header className="site-header">
-      <div className="logo-placeholder" aria-label="Left logo placeholder">
-        <img  className="college-logo" src="/src/assets/aiet-logo.png" alt="logo" />
-      </div>
+   <header className="site-header">
+  <div className="logo-placeholder" aria-label="Left logo placeholder">
+    <img className="college-logo" src={aietLogo} alt="AIET logo" />
+  </div>
 
-      <div className="header-title">
-        <h1>Alva's Institute of Engineering and Technology</h1>
-        <h3>A Unit of Alva's Education Foundation(R), Moodubidire</h3>
-        <h4>(Affilliated to VTU, Belgaum, Approved by AICTE, New Delhi, Recognized by Govt. of Karnataka)</h4>
-      </div>
+  <div className="header-title">
+    <h1>Alva's Institute of Engineering and Technology</h1>
+    <h3>A Unit of Alva's Education Foundation(R), Moodubidire</h3>
+    <h4>
+      Affiliated to VTU, Belgaum, Approved by AICTE, New Delhi,
+      Recognized by Govt. of Karnataka
+    </h4>
+  </div>
 
-      <div className="logo-placeholder" aria-label="Right logo placeholder">
-        <img  className="branch-logo" src="/src/assets/aiet-logo.png" alt="logo" />
-      </div>
-    </header>
+  <div className="logo-placeholder" aria-label="Right logo placeholder">
+    <img className="branch-logo" src={aietLogo} alt="Department logo" />
+  </div>
+</header>
   );
 }
 
