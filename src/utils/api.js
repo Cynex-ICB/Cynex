f7ce9f169dae3731b834aeb1159b61b34be3b0fa
cynexicb <cynex.ctf@gmail.com>
2026-05-28 12:15:27 +0530

    updated backend api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "https://api.cynexicb.com";
+  import.meta.env.VITE_API_URL || "https://api.cynexicb.com/api";
 
 export const API_ORIGIN = API_BASE_URL.replace(/\/api\/?$/, "");
 
