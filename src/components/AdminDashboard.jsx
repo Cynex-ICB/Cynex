9be20fe682a21c68beecbe5a4faddf439d6be20e
cynexicb <cynex.ctf@gmail.com>
2026-05-28 14:36:09 +0530

    made some changes to UI

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -4,7 +4,7 @@ import { API_BASE_URL, API_ORIGIN, readApiJson } from "../utils/api.js";
 
 const initialMaterialForm = {
   title: "",
-  category: "Notes",
+  category: "note",
   description: "",
   link: "",
   dueDate: "",
